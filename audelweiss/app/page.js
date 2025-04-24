81bcb9650229e8f93aab68852a8af80d14b434e9
Ethan Bourguigneau <ethan.devfs@pm.me>
2025-04-24 17:55:44 +0200

    feat : homePage section categorie en cours

--- audelweiss/app/page.js
+++ audelweiss/app/page.js
@@ -2,6 +2,7 @@
 
 import Link from "next/link";
 import {useEffect, useState} from "react";
+import AccessoiresHoverSection from "./components/AccessoiresHoverSection";
 
 export default function Home() {
     const [background, setBackground] = useState("");
@@ -38,6 +39,33 @@ export default function Home() {
             href: "https://audelweiss.fr/project/la-patate-positive/",
         },
     ];
+
+    const accessoiresCategory = {
+        accessoiresItems : [
+            {
+                key: "bandeaux",
+                label: "Bandeaux",
+                href: "/product-category/accessoires/bandeaux/",
+                image: "https://audelweiss.fr/wp-content/uploads/2025/02/bandeau-raye.jpg.webp",
+            },
+            {
+                key: "bonnets",
+                label: "Bonnets",
+                href: "/product-category/accessoires/bonnets/",
+                image: "https://audelweiss.fr/wp-content/uploads/2025/02/bonnet-avec-pompom.jpg.webp",
+            },
+            {
+                key: "accessoires",
+                label: "Accessoires",
+                href: "/product-category/accessoires/",
+                image: "https://audelweiss.fr/wp-content/uploads/2025/02/porte-cle.jpg.webp",
+            },
+        ],
+        buttonAction: {
+            label: "Découvrir les accessoires",
+            href: "/product-category/accessoires/",
+        }
+    };
 
     useEffect(() => {
         const updateBackground = () => {
@@ -82,46 +110,53 @@ export default function Home() {
 
             <section className="bg-white px-4 py-24">
                 <div className="flex flex-col lg:flex-row justify-center items-center gap-16 lg:gap-28">
-                    <article className="w-86 max-w-md -translate-y-10">
+                    <article className="w-86 max-w-md lg:-translate-y-10">
                         <div>
                             <span className="text-[#F6B99C] text-3xl">01</span>
                             <h2 className="w-5/6 uppercase text-2xl text-black mt-4">Artisanat embrunais</h2>
                             <p className="text-gray-700 mt-2 leading-7">
                                 Je vis dans les Hautes-Alpes, un cadre qui m’inspire chaque jour.
-                                Toutes mes créations sont réalisées ici, à la main, avec des matériaux choisis avec soin.
-                                J’aime l’idée de proposer des pièces qui portent en elles un peu de cette authenticité montagnarde.
+                                Toutes mes créations sont réalisées ici, à la main, avec des matériaux choisis avec
+                                soin.
+                                J’aime l’idée de proposer des pièces qui portent en elles un peu de cette authenticité
+                                montagnarde.
                             </p>
                         </div>
                     </article>
 
-                    <article className="w-86 max-w-md translate-y-10">
+                    <article className="w-86 max-w-md lg:translate-y-10">
                         <div>
                             <span className="text-[#F6B99C] text-3xl">02</span>
-                            <h2 className="w-5/6 uppercase font-normal text-2xl text-black mt-4">Éditions limitées ou sur-mesure</h2>
+                            <h2 className="w-5/6 uppercase font-normal text-2xl text-black mt-4">Éditions limitées ou
+                                sur-mesure</h2>
                             <p className="text-gray-700 mt-2 leading-7">
                                 Je suis une créatrice curieuse, toujours en quête de nouvelles idées.
                                 J’aime tester des techniques, des couleurs et des matières différentes.
-                                Cette envie d’explorer donne naissance à des pièces variées : certaines sont produites en petites séries,
+                                Cette envie d’explorer donne naissance à des pièces variées : certaines sont produites
+                                en petites séries,
                                 d’autres peuvent être personnalisées selon vos goûts et vos besoins.
                             </p>
                         </div>
                     </article>
 
-                    <article className="w-86 max-w-md -translate-y-10">
+                    <article className="w-86 max-w-md lg:-translate-y-10">
                         <div>
                             <span className="text-[#F6B99C] text-3xl">03</span>
-                            <h2 className="w-5/6 uppercase font-normal text-2xl text-black mt-4">Énergie et bien-être avec le Reiki</h2>
+                            <h2 className="w-5/6 uppercase font-normal text-2xl text-black mt-4">Énergie et bien-être
+                                avec le Reiki</h2>
                             <p className="text-gray-700 mt-2 leading-7">
                                 Depuis 2021, je suis certifiée praticienne Reiki.
-                                Chaque fois que je crée, je me connecte à cette énergie pour infuser mes pièces d’intentions positives.
-                                Mon but est de proposer des créations qui vous apportent à la fois bien-être et harmonie visuelle.
+                                Chaque fois que je crée, je me connecte à cette énergie pour infuser mes pièces
+                                d’intentions positives.
+                                Mon but est de proposer des créations qui vous apportent à la fois bien-être et harmonie
+                                visuelle.
                             </p>
                         </div>
                     </article>
                 </div>
             </section>
 
-            <section id="home-portfolio" className="bg-white px-10 md:px-40 py-20 relative z-0">
+            <section id="home-portfolio" className="bg-white px-10 md:px-30 py-20 relative z-0">
                 <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-8">
                     {main_products.map((proj, i) => (
                         <div
@@ -153,12 +188,12 @@ export default function Home() {
                                     }}
                                 >
                                     {/* Titre avec animation fade-down */}
-                                    <div className="bg-[#F6B99C] text-white px-4 py-2 w-56 shadow animate-fade-down">
-                                        <h2 className="text-2xl font-bold leading-tight">{proj.title}</h2>
+                                    <div className="bg-[#F6B99C] text-white px-4 py-2 min-w-54 w-full shadow animate-fade-down">
+                                        <h2 className="text-xl font-bold leading-tight">{proj.title}</h2>
                                     </div>
 
                                     {/* Catégorie avec animation fade-down-delay */}
-                                    <div className="bg-[#F6B99C] text-white px-4 py-2 w-40 animate-fade-down-delay">
+                                    <div className="bg-[#F6B99C] text-white px-4 py-2 w-40 animate-fade-down-delay uppercase">
                                         <p className="text-sm">{proj.category}</p>
                                     </div>
                                 </div>
@@ -167,6 +202,32 @@ export default function Home() {
                     ))}
                 </div>
             </section>
+
+            <section className="bg-[#fdf1eb] py-30">
+                <div className="container mx-auto px-4">
+                    <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
+                        {/* Section des images avec décalage */}
+                        <div className="relative flex justify-center space-y-4 col-span-1 md:col-span-1">
+                            <img
+                                className="w-48 h-[28rem] object-cover rounded-full shadow absolute -top-20 -left-10 z-10"
+                                src="https://audelweiss.fr/wp-content/uploads/2025/02/0b0bc07c-1615-4152-b893-770a637929dc.webp"
+                                alt="0b0bc07c-1615-4152-b893-770a637929dc"
+                            />
+                            <img
+                                className="w-80 h-[23rem] object-cover shadow absolute top-20 left-20 z-20"
+                                src="https://audelweiss.fr/wp-content/uploads/2025/02/bandeaufantaisie.jpg.webp"
+                                alt="bandeaufantaisie"
+                            />
+                        </div>
+
+                        {/* Section des accessoires qui prend plus de place */}
+                        <div className="col-span-1 md:col-span-2">
+                            <AccessoiresHoverSection items={accessoiresCategory.accessoiresItems} buttonItem={accessoiresCategory.buttonAction} />
+                        </div>
+                    </div>
+                </div>
+            </section>
+
         </div>
     );
 }
