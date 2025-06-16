e2fa3a60d643f538ac26fe0f63802a25852808ea
AyunGaming <brian.verchere@gmail.com>
2025-06-16 17:57:58 +0100

    Ajout collection ProductArticleDescription

---
+++ audelweiss-strapi/src/api/product-article-description/controllers/product-article-description.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * product-article-description controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::product-article-description.product-article-description');
---
+++ audelweiss-strapi/src/api/product-article-description/services/product-article-description.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * product-article-description service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::product-article-description.product-article-description');
