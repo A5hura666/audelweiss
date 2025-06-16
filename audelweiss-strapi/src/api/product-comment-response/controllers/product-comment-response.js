b610a7d92e3a87941b2c52e2e4906d2dda94b8a8
AyunGaming <brian.verchere@gmail.com>
2025-06-16 17:58:52 +0100

    Ajout collection ProductCommentResponse

---
+++ audelweiss-strapi/src/api/product-comment-response/controllers/product-comment-response.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * product-comment-response controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::product-comment-response.product-comment-response');
