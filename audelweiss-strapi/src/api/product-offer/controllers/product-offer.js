f88025550c1b7f3127c3765aa1c7443e67752d24
AyunGaming <brian.verchere@gmail.com>
2025-06-16 17:59:08 +0100

    Ajout collection ProductOffer

---
+++ audelweiss-strapi/src/api/product-offer/controllers/product-offer.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * product-offer controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::product-offer.product-offer');
