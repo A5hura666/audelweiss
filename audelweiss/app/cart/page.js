5a13456ca9947fe0553bb714631efebc2a90ae9a
Ethan Bourguigneau <ethan.devfs@pm.me>
2025-06-29 18:43:01 +0200

    Update page.js

--- audelweiss/app/cart/page.js
+++ audelweiss/app/cart/page.js
@@ -106,7 +106,7 @@ export default function CartPage() {
     const total = cart.reduce((sum, item) => sum + item.productPrice * item.quantity, 0);
 
     return (
-        <div className="max-w-5xl mx-auto px-6 py-12">
+        <div className="max-w-5xl mx-auto px-6 py-12 text-gray-700">
             <h1 className="text-4xl font-bold mb-10 text-center">Votre panier</h1>
 
             {cart.length === 0 ? (
