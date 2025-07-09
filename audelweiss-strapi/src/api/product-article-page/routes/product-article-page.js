4b69a0085a573bb0d2e7ce4becfb6c1200e3ec7f
AyunGaming <brian.verchere@gmail.com>
2025-07-09 22:18:28 +0100

    Ajout des composants pour les articles

---
+++ audelweiss-strapi/src/api/product-article-page/routes/product-article-page.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * product-article-page router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::product-article-page.product-article-page');
