c566d15850d684e7e47c7199d674c414b0b77611
AyunGaming <brian.verchere@gmail.com>
2025-07-09 22:19:28 +0100

    Ajout des composants pour le blog

---
+++ audelweiss-strapi/src/api/blog-article-page/routes/blog-article-page.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * blog-article-page router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::blog-article-page.blog-article-page');
